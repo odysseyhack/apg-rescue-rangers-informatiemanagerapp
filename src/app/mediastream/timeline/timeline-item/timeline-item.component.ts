cf40dbf4f4ad22fc8e361126111da3fe14f33d38
bart <bart-driessen@hotmail.com>
2019-04-13 21:58:53 +0200

    Information flow static data finished, map added back in.

--- src/app/mediastream/timeline/timeline-item/timeline-item.component.ts
+++ src/app/mediastream/timeline/timeline-item/timeline-item.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline-item.component.scss']
 })
 export class TimelineItemComponent implements OnInit {
-  title = "Vuur bij Landenstraat verspreidt richting school";
-  subtext = "Bericht geverifieerd door 22 gebruikers";
+  incomingMessages = [{title: "Vuur bij Landenstraat verspreidt richting school", message: "Bericht geverifieerd door 22 gebruikers.",timestamp: "23sec geleden"},
+  {title: "Omwonenden geevacueerd",message: "De omwonenden zijn inmiddels geevacueerd. Verwacht wordt dat ze de aankomende paar dagen niet huiswaarts kunnen gaan.",timestamp: "20min geleden"},
+  {title: "Brandweer gearriveerd", message: "De brandweer is momenteel ter plaatse. Het vuur is inmiddels verspreid richting buurwoning.", timestamp: "1u geleden"},  
+  {title: "Vuur gedetecteerd Landenstraat", message: "Om 21:15u is een brand ontdekt in de Landenstraat. Brandweer is gealarmeerd.",timestamp: "1u23min geleden"}
+];
 
   constructor() { }
 
